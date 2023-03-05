32ad0bc3f2e19117648c68edd2d471202d6d2cf4
Andikat Jacob Dennis <andikatjacobdennis@hotmail.com>
2023-03-05 17:07:44 +0530

    Added color to make it more attractive

--- ScientificCalculator/Program.cs
+++ ScientificCalculator/Program.cs
@@ -11,13 +11,15 @@ namespace ScientificCalculator
         static void Main()
         {
             // Display welcome message
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("===========================================================");
             Console.WriteLine("||            Welcome to the Scientific Calculator       ||");
             Console.WriteLine("===========================================================\n");
             Console.WriteLine("This program can evaluate arithmetic, trigonometric, and bracketed expressions.\n");
             Console.WriteLine("Sample usage: sin(2*(3+4))-1");
             while (true)
-            {             
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 // Get the math expression from the user
                 Console.Write("\nPlease enter the math expression (or type \"no\" to exit): ");
                 string mathExpression = Console.ReadLine();
@@ -31,16 +33,19 @@ namespace ScientificCalculator
                 // Evaluate the math expression
                 try
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     ShuntingYardAlgorithm shuntingYardAlgorithm = new ShuntingYardAlgorithm();
                     double result = shuntingYardAlgorithm.Evaluate(mathExpression);
                     Console.WriteLine($"Result: {result}\n");
                 }
                 catch (Exception ex)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Error: {ex.Message}\n");
                 }
             }
 
+            Console.ResetColor();
             Console.WriteLine("Thank you for using the Scientific Calculator! Press any key to exit...");
             Console.ReadKey();
         }
